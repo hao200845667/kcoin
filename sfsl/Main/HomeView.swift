6f3f3ea62559b1f6a92dcdb17fa62b2ae230606c
gongbihua <941078860@qq.com>
2020-05-20 10:14:40 +0800

    again

--- sfsl/Main/HomeView.swift
+++ sfsl/Main/HomeView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 //测试
+//测试Again
 struct HomeView: View {
     @State var showImage: Bool = true
     var banner_url: [String] = ["http://sfsl.huitongshiye.cn/uploads/images/2019/11-06/80ce71ae3907b43bbea2b1d9bcddfffc.jpg", "https://sifangsuanliimg.oss-cn-shanghai.aliyuncs.com/uploads/20191218/b917b08872e78a39c2b8ae7ca32eacb9.png","http://sfsl.huitongshiye.cn/uploads/images/2019/11-07/10d48d1123853f8d4e250d508a93c5e1.jpg","http://ww1.sinaimg.cn/orj480/736f0c7ejw1f659dt6n94j20bu06k754.jpg"]
